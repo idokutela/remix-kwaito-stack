c89c77b003ed0a99f7e482e004bde70893bdca18
Alexander Kahle <idokutela@gmail.com>
2023-05-31 15:26:40 +0200

    First with code

---
+++ cypress/e2e/smoke.cy.ts
@@ -0,0 +1,11 @@
+import { faker } from "@faker-js/faker";
+
+describe("smoke tests", () => {
+  afterEach(() => {
+    cy.cleanupUser();
+  });
+
+  it("should be a test", () => {
+  // TODO
+  });
+});
---
+++ scripts/migrate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Not sure it is necessary to create a swapfile:
+# afaik, the simple script should not want to allocate huge amounts of memory.
+
+# fallocate -l 512M /swapfile
+# chmod 0600 /swapfile
+# mkswap /swapfile
+# echo 10 >/proc/sys/vm/swappiness
+# swapon /swapfile
+# echo 1 >/proc/sys/vm/overcommit_memory
+
+# Remove prisma migrate
+# npx prisma migrate deploy
+
+# ... in favour of the migrate script
+ts-node -require tsconfig-paths/register scripts/migrate.ts
